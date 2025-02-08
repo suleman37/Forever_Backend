0b532069e8ed3c37105e31f0df8fc187edc0838d
suleman37 <sulemanshakeel37@gmail.com>
2025-02-08 15:17:12 +0500

    Add Cloudinary For Upload Images

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,5 @@
+import {v2 as cloudinary} from 'cloudinary';
+
 const AddProduct = async (req, res) => {
   try {
     const {
@@ -14,6 +16,17 @@ const AddProduct = async (req, res) => {
     const image3 = req.files.image3 ? req.files.image3[0] : null;
     const image4 = req.files.image4 ? req.files.image4[0] : null;
 
+    const images = [image1, image2, image3, image4].filter(
+      (item) => item !== undefined
+    );
+
+    const imagesUrl = await Promise.all(
+      images.map(async (item) => {
+        const result = await cloudinary.uploader.upload (item.path,{resource_type:'image'});
+        return result.secure_url;
+      })
+    );
+
     console.log(
       name,
       description,
@@ -23,11 +36,11 @@ const AddProduct = async (req, res) => {
       sizes,
       bestSeller
     );
-    console.log(image1, image2, image3, image4);
+    console.log(imagesUrl);
     res.json({});
   } catch (error) {
     console.log(error);
-    res.json({ success: false, message, message: error.message });
+    res.json({ success: false, message: error.message });
   }
 };
 
