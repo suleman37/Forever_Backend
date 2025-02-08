077e983110f554571f781f68395cbff82e8b1107
suleman37 <sulemanshakeel37@gmail.com>
2025-02-08 11:44:21 +0500

    Add Products and Images

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,38 @@
-
-
 const AddProduct = async (req, res) => {
-    
-}
+  try {
+    const {
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestSeller,
+    } = req.body;
+    const image1 = req.files.image1 ? req.files.image1[0] : null;
+    const image2 = req.files.image2 ? req.files.image2[0] : null;
+    const image3 = req.files.image3 ? req.files.image3[0] : null;
+    const image4 = req.files.image4 ? req.files.image4[0] : null;
 
+    console.log(
+      name,
+      description,
+      price,
+      category,
+      subCategory,
+      sizes,
+      bestSeller
+    );
+    console.log(image1, image2, image3, image4);
+    res.json({});
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message, message: error.message });
+  }
+};
 
-const listProduct = async (req, res) => {
-    
-}
-const removeProduct = async (req, res) => {
-    
-}
-const singleProduct = async (req, res) => {
-    
-}
+const listProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {};
 
-export {AddProduct , listProduct , removeProduct , singleProduct};
+export { AddProduct, listProduct, removeProduct, singleProduct };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -5,10 +5,20 @@ import {
   removeProduct,
   singleProduct,
 } from "../controllers/productController.js";
+import upload from "../middlewares/multer.js";
 
 const productRoute = express.Router();
 
-productRoute.post("/add", AddProduct);
+productRoute.post(
+  "/add",
+  upload.fields([
+    { name: "image1", maxCount: 1 },
+    { name: "image2", maxCount: 1 },
+    { name: "image3", maxCount: 1 },
+    { name: "image4", maxCount: 1 },
+  ]),
+  AddProduct
+);
 productRoute.get("/list", listProduct);
 productRoute.post("/remove", removeProduct);
 productRoute.post("/single", singleProduct);
