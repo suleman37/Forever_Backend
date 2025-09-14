0b2cefd12f7d34ce3579b2e499d8cabe60f0cfc8
Suleman Shakeel <sulemanshakeel37@gmail.com>
2025-09-15 00:08:22 +0500

    Remove commented-out admin authentication from product route

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -6,13 +6,11 @@ import {
   singleProduct,
 } from "../controllers/productController.js";
 import upload from "../middlewares/multer.js";
-// import adminAuth from "../middlewares/adminAuth.js";
 
 const productRoute = express.Router();
 
 productRoute.post(
   "/add",
-  // adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
